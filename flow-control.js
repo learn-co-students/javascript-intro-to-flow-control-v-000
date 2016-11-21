7d435a0f2f77dd09afb3e0c60eb96f6c7f522c38
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-11-21 13:07:47 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -25,4 +25,21 @@ function ageChecker(age) {
     }
   }
 
-  
+    function ternaryTeenager(age) {
+    if (age>=13 && age<=19) {
+      return "You are a teenager"
+    } else {
+      return "You are not a teenager"
+    }
+  }
+
+  function switchAge(age){
+  switch (age) {
+    case 15:
+      return "You are a teenager";
+      break;
+    default:
+    return "You have an age";
+
+    }
+  }
