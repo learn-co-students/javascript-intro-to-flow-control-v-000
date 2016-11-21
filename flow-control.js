60096e6c65fb299f65c2001c5f19112e3b17507e
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2016-11-21 12:38:40 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -13,3 +13,16 @@ function teenager(age) {
     return "You are not a teenager"
   }
 }
+
+
+function ageChecker(age) {
+  if (age>=13 && age<=19) {
+    return "You are a teenager!"
+    } else if (age<13) {
+      return "You are a kid"
+    } else {
+      return "You are a grownup"
+    }
+  }
+
+  
