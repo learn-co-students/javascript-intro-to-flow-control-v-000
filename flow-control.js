c62dd0ce5fb6aaa3654da13d1c37b8d7dc68dd3b
Tami Abrams <graces.daughter@gmail.com>
2016-11-23 21:59:52 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,1 +1,33 @@
-'use strict';
+function basicTeenager(age) {
+  if ((age >=13) && (age <= 19)) {
+    return "You are a teenager!"
+}
+}
+
+
+
+function teenager (age) {
+  if ((age >=13 ) && (age <=19))
+  {return "You are a teenager!"
+};
+  else
+  {
+  return "You are not a teenager!"
+}
+};
+
+age(12)
+
+function ageChecker (age) {
+  if ((age >=13) && (age <=19)){
+    return "You are a teenager!"
+  };
+else if (age <=12){
+    return "You are a kid"
+  };
+  else {
+    return "You are a grownup"
+  }
+
+
+}
