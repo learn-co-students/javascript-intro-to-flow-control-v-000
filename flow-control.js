9cd40acc157e74c58eee36850557108bf2ad6b72
Hilary Lyttle <hilyttle@gmail.com>
2017-01-02 18:57:48 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -8,19 +8,19 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else {
-    return "You are not a teenager"
+    return "You are not a teenager";
   }
 }
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else if (age <= 12) {
-    return "You are a kid"
+    return "You are a kid";
   } else {
-    return "You are a grownup"
+    return "You are a grownup";
   }
 }
 
